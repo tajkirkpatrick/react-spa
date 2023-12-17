b5e14bb2815b70b3433a184fb0c13a775ce6f4cc
tajkirkpatrick <50558516+tajkirkpatrick@users.noreply.github.com>
2023-12-17 06:39:41 -0600

    refactor: added combobox selector

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:@typescript-eslint/recommended",
--- src/shared/Header.tsx
+++ src/shared/Header.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import {
   Sheet,
   SheetContent,
@@ -16,15 +18,58 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import TypographyP from "@/shared/TypographyP.tsx";
 
 import { ModeToggle } from "@/components/mode-toggle";
 
 import { Link } from "@tanstack/react-router";
 
+type Status = {
+  value: string;
+  label: string;
+};
+
+const statuses: Status[] = [
+  {
+    value: "backlog",
+    label: "Backlog",
+  },
+  {
+    value: "todo",
+    label: "Todo",
+  },
+  {
+    value: "in progress",
+    label: "In Progress",
+  },
+  {
+    value: "done",
+    label: "Done",
+  },
+  {
+    value: "canceled",
+    label: "Canceled",
+  },
+];
+
 function Header() {
+  const [open, setOpen] = useState(false);
+  const [selectedStatus, setSelectedStatus] = useState<Status | null>(null);
   return (
     <>
       <nav className="container mx-auto flex h-[8svh] w-screen items-center justify-between py-4">
@@ -46,11 +91,57 @@ function Header() {
                   <DialogHeader>
                     <DialogTitle>Submit A Ticket</DialogTitle>
                     <DialogDescription>
-                      <Input
-                        className="my-4"
-                        placeholder="Search By Player Name"
-                      />
-                      <div className="flex w-full items-center justify-end gap-x-2">
+                      <div className="mt-4 flex items-center gap-x-4">
+                        <p className="whitespace-nowrap leading-7 [&:not(:first-child)]:mt-6 ">
+                          Player Name:{" "}
+                        </p>
+                        <Popover open={open} onOpenChange={setOpen}>
+                          <PopoverTrigger asChild>
+                            <Button
+                              variant="outline"
+                              className="w-full justify-start"
+                            >
+                              {selectedStatus ? (
+                                <>{selectedStatus.label}</>
+                              ) : (
+                                <>+ Set status</>
+                              )}
+                            </Button>
+                          </PopoverTrigger>
+                          <PopoverContent
+                            className="p-0"
+                            side="right"
+                            align="start"
+                          >
+                            <Command>
+                              <CommandInput placeholder="Change status..." />
+                              <CommandList>
+                                <CommandEmpty>No results found.</CommandEmpty>
+                                <CommandGroup>
+                                  {statuses.map((status) => (
+                                    <CommandItem
+                                      key={status.value}
+                                      value={status.value}
+                                      onSelect={(value) => {
+                                        setSelectedStatus(
+                                          statuses.find(
+                                            (priority) =>
+                                              priority.value === value,
+                                          ) || null,
+                                        );
+                                        setOpen(false);
+                                      }}
+                                    >
+                                      {status.label}
+                                    </CommandItem>
+                                  ))}
+                                </CommandGroup>
+                              </CommandList>
+                            </Command>
+                          </PopoverContent>
+                        </Popover>
+                      </div>
+                      <div className="mt-6 flex w-full items-center justify-end gap-x-2">
                         <DialogClose>
                           <Button variant="destructive">Cancel</Button>
                         </DialogClose>
